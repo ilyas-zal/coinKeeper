1c356f7997c34d488533368f83ee9c009d413d36
Emphyriw <emphyriw@Zal-Aorus.>
2025-02-25 16:49:53 +0300

    ver 0.0001

---
+++ cmd/coinKeeper/main.go
@@ -0,0 +1,23 @@
+package main
+
+/*
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	appTest := app.New()
+	wind := appTest.NewWindow("Lets Go BROOO")
+
+	label := widget.NewLabel("Lets Go BROOO\n И вот так могет!")
+
+	wind.SetContent(container.NewVBox(
+		label,
+	))
+
+	wind.ShowAndRun()
+
+}
+*/
